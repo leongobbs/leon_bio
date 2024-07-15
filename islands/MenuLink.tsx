b349bb0888ed33f0311debaf6cf361cbf63f2d34
i.nizamov <ireknzaimov94@gmail.com>
2024-07-16 01:08:04 +0300

    created blog files

--- islands/MenuLink.tsx
+++ islands/MenuLink.tsx
@@ -1,26 +1,25 @@
+
 import { useState, useEffect } from 'preact/hooks';
 
 export default function MenuLink() {
-  // Создаем состояние для хранения текущего URL
   const [href, setHref] = useState("");
 
-  // Хук useEffect выполняется после монтирования компонента
   useEffect(() => {
-    // Проверяем, выполняется ли код в браузере
+    const currentHref = window.location.href;
+    console.log("Current Href:", currentHref); // Логируем текущий URL
     if (typeof window !== "undefined") {
-      // Обновляем состояние с текущим URL
-      setHref(window.location.href);
+      setHref(currentHref);
+    } else {
+      console.error("Window is undefined"); // Логируем ошибку, если window недоступен
     }
   }, []);
 
-  // Определяем ссылку и текст ссылки в зависимости от текущего URL
-  const link = href.endsWith("/") || href.endsWith("/index.html") ? "/about" : "/";
-  const linkText = href.endsWith("/") || href.endsWith("/index.html") ? "About" : "Home";
+  const basePath = "/"; // Обновите базовый путь, если необходимо
+  const link = href.endsWith("/") || href.endsWith(`${basePath}index.html`) ? `${basePath}about` : basePath;
+  const linkText = href.endsWith("/") || href.endsWith(`${basePath}index.html`) ? "About" : "Home";
 
-  // Возвращаем JSX разметку
   return (
     <div class="text-right text-m">
-      {/* Создаем ссылку с подчеркиванием и соответствующим URL и текстом */}
       <a class="underline" href={link}>{linkText}</a>
     </div>
   );
